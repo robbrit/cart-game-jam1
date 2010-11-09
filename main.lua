43574e7b1de4cdc6fed9b61ed7c6b40bcdc8c6b7
Ramy D <ramy.daghstani@gmail.com>
2010-11-08 21:28:42 -0500

    my changes to main

--- main.lua
+++ main.lua
@@ -1,16 +1,29 @@
+require "explosions.lua"
+
+function explosion(x, y)
+    image = love.graphics.newImage("part1.jpg") 
+    buffer = 20
+    A = love.graphics.newParticleSystem( image, buffer)
+    A:setSpin(1, 2, 0.5)
+    A:setPosition(x, y)
+    A:setLifetime(3)
+    --[[love.graphics.draw(A, x, y)]]--
+    print("exploding")
+end
+
 function love.load()
    music = love.audio.newSource("02 - Let It Die.mp3") -- if "static" is omitted, LÖVE will stream the file from disk, good for longer music tracks
    local f = love.graphics.newFont(12)
    love.audio.play(music)
    love.graphics.setFont(f)
    love.graphics.setBackgroundColor(255,255,255)
-   
+   explosion(50, 50)
 end
 
 function love.update(dt)
     mouseX, mouseY = love.mouse.getPosition()
-    print("mouseX: " .. mouseX)
-    print("mouseY: " .. mouseY)
+   --[[ print("mouseX: " .. mouseX)]]--
+   --[[ print("mouseY: " .. mouseY)]]--
 end
 
 function love.draw()
@@ -22,7 +35,6 @@ function love.mousepressed(x, y, button)
       imgx = x -- move image to where mouse clicked
       imgy = y
    end
-      love.graphics.draw(image, imgx, imgy)
 end
 
 function love.mousereleased(x, y, button)
@@ -41,6 +53,8 @@ end
 
 function love.keyreleased(key, unicode)
    if key == 'b' then
+       
+   explosion(50, 50)
       print("b released")
    elseif key == 'a' then
       print("a released")

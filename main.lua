0308c7317a2949d207750bd9636684da3bc21289
Rob <rob.s.brit@gmail.com>
2010-11-08 21:08:45 -0500

    Made chicken move.

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 require "enemy.lua"
 
 enemies = {}
+MAXFPS = 30
 
 function love.load()
-  enemies[0] = Enemy(400, 0, love.graphics.newImage("chicken.png"))
+	math.randomseed( os.time() )
+  enemies[0] = Enemy(love.graphics.newImage("chicken.png"))
 
   music = love.audio.newSource("02 - Let It Die.mp3") -- if "static" is omitted, LÖVE will stream the file from disk, good for longer music tracks
   local f = love.graphics.newFont(12)
@@ -14,42 +16,49 @@ end
 
 function love.draw()
   for i, enemy in pairs(enemies) do
-    love.graphics.draw(enemy["image"], enemy["x"], enemy["y"])
+    love.graphics.draw(enemy["image"], math.floor(enemy["x"]), math.floor(enemy["y"]))
   end
 end
 
 function love.update(dt)
-    mouseX, mouseY = love.mouse.getPosition()
-    print("mouseX: " .. mouseX)
-    print("mouseY: " .. mouseY)
+  local ms = (1000 / MAXFPS) - (dt * 1000)
+  if ms > 0 then
+    love.timer.sleep(ms)
+  end
+  mouseX, mouseY = love.mouse.getPosition()
+
+  for i, enemy in pairs(enemies) do
+    updateEnemy(enemy, ms)
+  end
+
 end
 
 function love.mousepressed(x, y, button)
-   if button == 'l' then
-      imgx = x -- move image to where mouse clicked
-      imgy = y
-   end
-      love.graphics.draw(image, imgx, imgy)
+  if button == 'l' then
+    imgx = x -- move image to where mouse clicked
+    imgy = y
+  end
+  love.graphics.draw(image, imgx, imgy)
 end
 
 function love.mousereleased(x, y, button)
-   if button == 'l' then
-      print("hello")
-   end
+  if button == 'l' then
+    print("hello")
+  end
 end
 
 function love.keypressed(key, unicode)
-   if key == 'b' then
-      print("b pressed")
-   elseif key == 'a' then
-      print("a pressed")
-   end
+  if key == 'b' then
+    print("b pressed")
+  elseif key == 'a' then
+    print("a pressed")
+  end
 end
 
 function love.keyreleased(key, unicode)
-   if key == 'b' then
-      print("b released")
-   elseif key == 'a' then
-      print("a released")
-   end
+  if key == 'b' then
+    print("b released")
+  elseif key == 'a' then
+    print("a released")
+  end
 end
